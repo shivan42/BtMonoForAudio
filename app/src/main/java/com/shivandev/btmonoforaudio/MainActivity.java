78064388d3e197c746e0dc20092002ae0107622c
IvanSh <github@1cadapter.ru>
2014-11-10 22:59:29 +0300

    Готова основа - запуск и остановка вывода аудио потока на моно гарнитуру, поддерживается это сервисом, в котором идет мониторинг отключения вывода аудиопотока (чтобы завершить все что нужно) и входящих/исходящих звонков, чтобы переподключить потом вывод аудиопотока.
    далее попробуем вынести ресиверы наружу и сделать сервис отслеживающий подключение гарнитуры

---
+++ app/src/main/java/com/shivandev/btmonoforaudio/MainActivity.java
@@ -0,0 +1,55 @@
+package com.shivandev.btmonoforaudio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.onBtn).setOnClickListener(this);
+        findViewById(R.id.offBtn).setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.onBtn:
+                startService(ScoProcessingSrv.createStartScoIntent(getApplicationContext()));
+                break;
+            case R.id.offBtn:
+//                stopService(ScoProcessingSrv.createStopScoIntent(getApplicationContext()));
+                startService(ScoProcessingSrv.createStopScoIntent(getApplicationContext()));
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/shivandev/btmonoforaudio/ScoProcessingSrv.java
@@ -0,0 +1,194 @@
+package com.shivandev.btmonoforaudio;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.os.Handler;
+import android.os.IBinder;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class ScoProcessingSrv extends Service {
+
+    private static final boolean isDebugThisModule = true;
+    public static final String EXTRA_MODE = "EXTRA_MODE";
+    private int oldMediaVolume = -1;
+    private int oldBtVolume = -1;
+    private AudioManager mAudioManager;
+    private ScoStateUpdatedBCastRec mScoStateUpdatedBCastRec;
+    private BroadcastReceiver phoneCallListenerRec = null;
+    private Handler handler;
+    private boolean isScoOn;
+
+    public static enum Mode {
+        START_SCO,
+        STOP_SCO;
+    }
+
+    public static Intent createStartScoIntent(Context context) {
+        Intent intent = new Intent(context, ScoProcessingSrv.class);
+        intent.putExtra(EXTRA_MODE, Mode.START_SCO);
+        return intent;
+    }
+
+    public static Intent createStopScoIntent(Context context) {
+        Intent intent = new Intent(context, ScoProcessingSrv.class);
+        intent.putExtra(EXTRA_MODE, Mode.STOP_SCO);
+        return intent;
+    }
+
+    @Override
+    public void onCreate() {
+        handler = new Handler();
+        mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        mScoStateUpdatedBCastRec = new ScoStateUpdatedBCastRec();
+//        phoneCallListenerRec = new BluetoothStateBCastRec();
+//        registerReceiver(phoneCallListenerRec, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+
+    }
+
+    @Override
+    public void onDestroy() {
+        stopSCO();
+        if (phoneCallListenerRec != null) {
+            unregisterReceiver(phoneCallListenerRec);
+//            phoneCallListenerRec = null;
+        }
+//        stopForeground(true);
+        }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        isScoOn = mAudioManager.isBluetoothScoOn();
+        if (intent != null) {
+            Mode modeCommand = ((Mode) intent.getSerializableExtra(EXTRA_MODE));
+            switch (modeCommand) {
+                case START_SCO:
+                    startSco();
+                    break;
+                case STOP_SCO:
+                    stopSelf();
+//                    stopSCO();
+                    break;
+            }
+        }
+        return Service.START_STICKY;
+    }
+
+    private void startSco() {
+//        oldMediaVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+//        oldBtVolume = mAudioManager.getStreamVolume(6);
+        mAudioManager.startBluetoothSco();
+        isScoOn = true;
+        registerReceiver(mScoStateUpdatedBCastRec, new IntentFilter(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED));
+    }
+
+    private void stopSCO() {
+        /*
+        removeScoWatcher();
+        stopForeground(true);
+        mAudioManager.setStreamVolume(3, this.old_media_volume, 0);
+        mAudioManager.setStreamVolume(6, this.old_bt_volume, 0);
+        */
+        isScoOn = false;
+        unregisterReceiver(mScoStateUpdatedBCastRec);
+        mAudioManager.setMode(AudioManager.MODE_NORMAL);
+        mAudioManager.stopBluetoothSco();
+        mAudioManager.setBluetoothScoOn(false);
+        log("STOP BluetoothSco");
+        /*
+        this.mNM.cancel(1001);
+        localMBTApplication.status = 1;
+        localMBTApplication.notifyChangeListener();
+        localMBTApplication.showToast(getApplicationContext(), l(2131230732));
+        */
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    void log(String str) {
+        if (isDebugThisModule) Log.e("SCO Service", str);
+    }
+
+    /**
+     * отслеживаем состояние блютуса, и реагируем на выключение прекращением работы сервиса и SCO
+     */
+    class BluetoothStateBCastRec extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String str = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+            if ((TelephonyManager.EXTRA_STATE_RINGING.equals(str)) || (TelephonyManager.EXTRA_STATE_OFFHOOK.equals(str))) {
+                if (isScoOn)stopSCO();
+            }
+            if (TelephonyManager.EXTRA_STATE_IDLE.equals(str)) {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startSco();
+
+                    }
+                }, 2000);
+            }
+//            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+//            switch (state) {
+//                case BluetoothAdapter.STATE_OFF:
+//                    stopSCO();
+//                    break;
+//            }
+        }
+    }
+
+    class ScoStateUpdatedBCastRec extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int state = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, AudioManager.SCO_AUDIO_STATE_ERROR);
+            switch (state) {
+                case AudioManager.SCO_AUDIO_STATE_CONNECTED:
+                    log("SCO_AUDIO_STATE_CONNECTED");
+                    /*
+                    startForeground(SERVICE_ID, this.mBuilder.getNotification());
+                    if (MBTPreferences.media_volume != -1) {
+                        mAudioManager.setStreamVolume(3, MBTPreferences.media_volume, 0);
+                    }
+
+                    if (MBTPreferences.bluetooth_volume != -1) {
+                        mAudioManager.setStreamVolume(6, MBTPreferences.bluetooth_volume, 0);
+                    }
+                    */
+                    mAudioManager.setMode(AudioManager.MODE_IN_CALL);
+                    mAudioManager.setBluetoothScoOn(true);
+                    if (phoneCallListenerRec == null) {
+                        phoneCallListenerRec = new BluetoothStateBCastRec();
+                        registerReceiver(phoneCallListenerRec, new IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED));
+                    }
+
+//                    unregisterReceiver(this);
+                    /*
+                    showStatusMessage(0);
+                    localMBTApplication.notifyChangeListener();
+                    localMBTApplication.showToast(getApplicationContext(), l(2131230734));
+                    */
+                    //                playStartSound();
+                    //                        getApplicationContext().sendBroadcast(new Intent("com.android.music.musicservicecommand.togglepause"));
+                    //                    setupScoWatcher();
+                    //                mAudioManager.setStreamVolume(3, mAudioManager.getStreamMaxVolume(3), 0);
+                    break;
+                case AudioManager.SCO_AUDIO_STATE_DISCONNECTED:
+                    log("SCO_AUDIO_STATE_DISCONNECTED");
+//                    stopSCO();
+                    stopSelf();
+                    break;
+                case AudioManager.SCO_AUDIO_STATE_ERROR:
+                    log("SCO_AUDIO_STATE_ERROR");
+                    break;
+            }
+        }
+    }
+}
